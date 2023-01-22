31419c00fbe0bf677f5d6446696f6a98a41fe8a0
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-01-22 17:13:17 +0200

    Creating routes and layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from "./App";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import RootLayout from './pages/RootLayout';
+import Add from './pages/Add';
+import Edit from './pages/Edit';
+import Details from './pages/Details';
+import Index from './pages/Index';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Index /> },
+      { path: 'post', element: <Index /> },
+      { path: 'post/add', element: <Add /> },
+      {
+        path: 'post/:id',
+        element: <Details />,
+        loader: ({ params }) => {
+          if (isNaN(params.id)) {
+            throw new Response('Bad Request', {
+              statusText: 'please make sure to insert correct post ID',
+              status: 400,
+            });
+          }
+        },
+      },
+      { path: 'post/:id/edit', element: <Edit /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
